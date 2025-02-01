5f8cefd1f4373bf1ccec9aa304e86bb7cde7437a
user <user@MacBook-Pro-user.local>
2025-02-01 22:28:55 +0200

    Refactor

--- day-01/solution.js
+++ day-01/solution.js
@@ -3,24 +3,19 @@
 // --- Part One ---
 import fs from 'node:fs'
 
-// 1. Convert input to array
+// Convert input to array
 const input = fs.readFileSync('./day-01/input.txt', 'utf8', (err, data) => data)
 const convertedInput = input.split('\n').map((pair) => pair.split(/\s+/))
 
-// 3. Sort left and right halfs of input independently
+// Sort left and right halfs of input independently
 const sortedInputLeft = convertedInput.map((value) => value[0]).toSorted((a, b) => a - b)
 const sortedInputRight = convertedInput.map((value) => value[1]).toSorted((a, b) => a - b)
 
-// 2. Get distance
+// Get distance
 function findTotalDistance(leftHalf, rightHalf) {
-  const sortedInput = []
-  for (let i = 0; i < leftHalf.length; i++) {
-    sortedInput.push([leftHalf[i], rightHalf[i]])
-  }
-
   let totalDistance = 0
-  for (let i = 0; i < sortedInput.length; i++) {
-    totalDistance += Math.abs(sortedInput[i][0] - sortedInput[i][1])
+  for (let i = 0; i < leftHalf.length; i++) {
+    totalDistance += Math.abs(leftHalf[i] - rightHalf[i])
   }
   return totalDistance
 }
@@ -30,19 +25,19 @@ console.log(totalDistance) // 1603498
 
 // --- Part Two ---
 function getSimilarityScore(leftHalf, rightHalf) {
-  const leftNumbersSimilarityScore = new Map()
+  // Set similarity score for each left number
+  const leftNumbersFrequency = new Map()
   for (const leftNumber of leftHalf) {
     for (const rightNumber of rightHalf) {
       if (leftNumber === rightNumber) {
-        // prettier-ignore
-        leftNumbersSimilarityScore.set(leftNumber, (leftNumbersSimilarityScore.get(leftNumber) ?? 0) + 1)
+        leftNumbersFrequency.set(leftNumber, (leftNumbersFrequency.get(leftNumber) ?? 0) + 1)
       }
     }
   }
 
   const leftNumbersScore = []
   for (const number of leftHalf) {
-    leftNumbersScore.push(parseInt(number) * (leftNumbersSimilarityScore.get(number) ?? 0))
+    leftNumbersScore.push(parseInt(number) * (leftNumbersFrequency.get(number) ?? 0))
   }
 
   const similarityScore = leftNumbersScore.reduce((a, b) => a + b, 0)
